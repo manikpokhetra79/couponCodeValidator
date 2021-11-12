671effa35203b6cb42c7c71cdd007abff7b6f1eb
Manik Gupta <manikpokhetra@gmail.com>
2021-11-12 23:28:27 +0530

    coupon code validation working fine

--- controllers/apiControllers/counponsController.js
+++ controllers/apiControllers/counponsController.js
@@ -3,13 +3,13 @@ let Coupon = require('../../models/couponsSchema');
 module.exports.createCoupon = async (req, res) => {
   try {
     let name = req.body.name.toUpperCase();
-    console.log('inhere try');
+    // console.log('inhere try');
     let newCoupon = await Coupon.findOne({ name });
     let today = new Date();
-    let date = today.getDate();
+    // let date = today.getDate();
     if (newCoupon) {
       // coupon already exists
-      console.log('inhere');
+      // console.log('inhere');
       return res.status(400).json({
         message: 'coupon already exists',
         coupon: newCoupon,
@@ -49,5 +49,65 @@ module.exports.createCoupon = async (req, res) => {
     });
   }
 };
-// module.exports.validateCoupon = (req, res) => {};
+module.exports.validateCoupon = async (req, res) => {
+  try {
+    let code = req.body.couponcode.toUpperCase();
+    let newCoupon = await Coupon.findOne({ name: code });
+    let today = new Date();
+    let expirationDate = new Date(newCoupon.expirationDate);
+    let currentCartValue = req.body.cartValue;
+    if (!newCoupon) {
+      return res.status(422).json({
+        message: 'Invalid Coupon code',
+      });
+    } else {
+      // if coupon exists
+      // conditions when coupon won't work
+      // when coupon is past expiration date
+      if (today.getTime() > expirationDate.getTime()) {
+        return res.status(422).json({
+          message: 'coupon code past expiration date',
+          status: 'coupon expired',
+        });
+      } else if (currentCartValue < newCoupon.minCartAmount) {
+        // if current cartvalue is less than the required cart value
+        return res.status(422).json({
+          message: `Coupon will work for cart Value of over ${newCoupon.minCartAmount}`,
+          status: 'coupon code not applicable',
+        });
+      } else {
+        let couponType = newCoupon.couponType;
+        if (couponType === 'flat') {
+          let discountAmount = newCoupon.flatAmount;
+          let updatedCartValue =
+            (await currentCartValue) - newCoupon.flatAmount;
+          return res.status(200).json({
+            message: 'coupon code applied successfully',
+            discountAmount,
+            updatedCartValue,
+          });
+        } else {
+          let discountPercent = newCoupon.percentValue;
+          let discountAmount =
+            (await currentCartValue) * (discountPercent / 100);
+          if (discountAmount > newCoupon.percentMaxDiscount) {
+            discountAmount = newCoupon.percentMaxDiscount;
+          }
+          let updatedCartValue = (await currentCartValue) - discountAmount;
+          return res.status(200).json({
+            message: 'coupon code applied successfully',
+            discountAmount,
+            updatedCartValue,
+          });
+        }
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: 'Error while validating coupon',
+      error,
+    });
+  }
+};
 // module.exports.listCoupons = (req, res) => {};
--- routes/api/coupons.js
+++ routes/api/coupons.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const couponsController = require('../../controllers/apiControllers/counponsController');
 router.post('/create', couponsController.createCoupon);
-// router.get('/validate', couponsController.validateCoupon);
+router.get('/validate', couponsController.validateCoupon);
 // router.get('/listall', couponsController.listCoupons);
 module.exports = router;
