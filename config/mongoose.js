517e53bf59d430d81d3bd6af252c1232aa16cd22
Manik Gupta <manikpokhetra@gmail.com>
2021-11-13 21:48:30 +0530

    updated mongodb config for mongodb atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/couponsGeneratorDB');
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'DB error'));
-db.once('open', () => {
-  console.log('Database connected successfully at: ', process.env.PORT);
-});
-module.exports = db;
+const uri = process.env.COUPON_URI;
+const db = mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to Atlas database ');
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+  });
+
+// // mongoose.connect('mongodb://localhost/couponsGeneratorDB');
+// // const db = mongoose.connection;
+// // db.on('error', console.error.bind(console, 'DB error'));
+// // db.once('open', () => {
+// //   console.log('Database connected successfully at: ', process.env.PORT);
+// // });
+// // module.exports = db;
